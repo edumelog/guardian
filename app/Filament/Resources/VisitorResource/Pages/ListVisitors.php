aef411224d6742aad81927d6526f0e09f34ede52
Eduardo Melo <edumelog@gmail.com>
2025-02-21 22:46:46 +0000

    Cor do botao Dar entrada corrigida

--- app/Filament/Resources/VisitorResource/Pages/ListVisitors.php
+++ app/Filament/Resources/VisitorResource/Pages/ListVisitors.php
@@ -13,7 +13,9 @@ class ListVisitors extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->label('Registrar Entrada')
+                ->color('success'),
         ];
     }
 }
